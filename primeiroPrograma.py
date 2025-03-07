29026f0037b45b3a4424dfcb61704be4193e4852
juvedibuar00 <batchijuve@gmail.com>
2025-03-07 16:00:16 -0300

    novo commit

--- primeiroPrograma.py
+++ primeiroPrograma.py
@@ -4,7 +4,10 @@ print("Ola, mundo!")
 nome = "Lariane"
 print(nome)
 
+# Usando a função input() para receber uma mensagem do usuário
 saudacao = str(input("Qual é o teu nome? "))
 
+# Usando a função print() para exibir a mensagem recebida   
 print(f'Prazer em te conhecer {saudacao}')
+
 
