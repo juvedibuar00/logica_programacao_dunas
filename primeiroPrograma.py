6a5773552d5221f9928626011a81b133b6146ab6
JUVENALDO FLORENTINO CANJA <61490327363@professores.unifanor.edu.br>
2025-03-07 11:14:37 -0300

    novo commit

---
+++ primeiroPrograma.py
@@ -0,0 +1,10 @@
+#Escrevendo a primeira mensagem, usando a função print()
+print("Ola, mundo!")
+
+nome = "Lariane"
+print(nome)
+
+saudacao = str(input("Qual é o teu nome? "))
+
+print(f'Prazer em te conhecer {saudacao}')
+
